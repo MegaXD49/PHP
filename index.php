64998d0ac5173e649e99fd42bafb5c3e5e055a8b
Mega <138886362+MegaXD49@users.noreply.github.com>
2024-06-05 21:13:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+    </head>
+    <body>
+        <h1>
+        <?php echo "PHP & MariaDB"; ?>
+        </h1>
+
+        <h1 style="color:blue;">
+            <?php echo "สิ่งที่ต้องติดตั้ง:"; ?>
+        </h1>
+
+
+        <?php
+            $string = ["Apache", "PHP", "MariaDB"];
+
+            echo "<li>$string[0]</li>";
+            echo "<li>$string[1]</li>";
+            echo "<li>$string[2]</li>";
+
+        ?>
+
+        <?php
+            $p = "ค้นหา: ";
+            $p = "";
+            $p = "";
+            $p = "";
+            $p = "";
+        ?>
+
+
+
+<?php
+        $p = "php marinadb";
+        $p = "1";
+        $p = "10";
+        $p = "1000";
+
+        echo "<b>ผลลัพธที่ได้:</b>"."</br>";
+        echo "ค้นหา: ".$p." ผลลัพธ์ลำดับที่ ".$b." - ".$n." จากทั้งหมด ".$t;
+    ?>
+    
+    </body>
+</html>
---
+++ index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP</title>
+    </head>
+    <?php echo "<h1>PHP & MariaDB</h1>"; ?>
+    <?php echo "<h1 style='color:blue;'>สิ่งที่ต้องติดตั้ง:</h1>"; ?>
+    <?php
+        echo "<li>Apache</li>";
+        echo "<li>PHP</li>";
+        echo "<li>MariaDB</li>";
+    ?>
+    </body>
+</html>
